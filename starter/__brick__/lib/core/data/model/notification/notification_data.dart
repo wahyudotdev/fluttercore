94e8f5bc63add974812ad63c4a765f6f56f3cf7f
Wahyu Hidayat <wahyu.smallker@gmail.com>
2022-07-24 22:11:36 +0700

    add notification helper

---
+++ starter/__brick__/lib/core/data/model/notification/notification_data.dart
@@ -0,0 +1,72 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:test_template/core/utils/json_custom_parser.dart';
+part '../../../../gen/core/data/model/notification/notification_data.g.dart';
+
+@JsonSerializable()
+@JsonIntToBool()
+class NotificationData extends Equatable {
+  @JsonKey(name: 'id')
+  final String? id;
+
+  @JsonKey(name: 'created_at')
+  final DateTime? createdAt;
+
+  @JsonKey(name: 'updated_at')
+  final DateTime? updatedat;
+
+  @JsonKey(name: 'users_id')
+  final String? userId;
+
+  @JsonKey(name: 'type')
+  final String? type;
+
+  @JsonKey(name: 'title')
+  final String? title;
+
+  @JsonKey(name: 'message')
+  final String? message;
+
+  @JsonKey(name: 'description')
+  final String? description;
+
+  @JsonKey(name: 'parent_id')
+  final String? parentId;
+
+  @JsonKey(name: 'parent_detail')
+  final String? parentDetail;
+
+  @JsonKey(name: 'is_read')
+  final bool? isRead;
+
+  const NotificationData({
+    this.id,
+    this.createdAt,
+    this.updatedat,
+    this.userId,
+    this.type,
+    this.title,
+    this.description,
+    this.parentId,
+    this.parentDetail,
+    this.isRead,
+    this.message,
+  });
+
+  factory NotificationData.fromJson(Map<String, dynamic> json) =>
+      _$NotificationDataFromJson(json);
+
+  Map<String, dynamic> toJson() => _$NotificationDataToJson(this);
+
+  @override
+  List<Object?> get props => [
+        id,
+        createdAt,
+        updatedat,
+        userId,
+        type,
+        title,
+        parentId,
+        message,
+      ];
+}
---
+++ starter/__brick__/lib/core/utils/notification_service.dart
@@ -0,0 +1,68 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:test_template/core/config/config.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:test_template/core/data/model/notification/notification_data.dart';
+
+class NotificationService {
+  static final _notifications = FlutterLocalNotificationsPlugin();
+
+  static Future _notificationDetails() async {
+    return const NotificationDetails(
+      android: AndroidNotificationDetails(
+        Config.notificationChannelId,
+        Config.notificationChannelName,
+        importance: Importance.max,
+        enableLights: true,
+      ),
+      iOS: IOSNotificationDetails(),
+    );
+  }
+
+  static Future<void> showNotification({
+    int id = 100,
+    String? title,
+    String? body,
+    String? payload,
+  }) async =>
+      await _notifications.show(
+        id,
+        title,
+        body,
+        await _notificationDetails(),
+        payload: payload,
+      );
+
+  static Future<NotificationData?> setup() async {
+    const androidInitializationSettings =
+        AndroidInitializationSettings('ic_notification');
+    final IOSInitializationSettings initializationSettingsIOS =
+        IOSInitializationSettings(
+      requestSoundPermission: false,
+      requestBadgePermission: false,
+      requestAlertPermission: false,
+      onDidReceiveLocalNotification: (p0, p1, p2, p3) async {
+        debugPrint('$p0 $p1 $p2 $p3');
+      },
+    );
+    final initializationSettings = InitializationSettings(
+        android: androidInitializationSettings, iOS: initializationSettingsIOS);
+    _notifications.initialize(initializationSettings);
+    await _notifications
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(const AndroidNotificationChannel(
+            Config.notificationChannelId, Config.notificationChannelName));
+
+    final notificationData =
+        await _notifications.getNotificationAppLaunchDetails();
+    if (notificationData?.payload != null) {
+      final data =
+          NotificationData.fromJson(jsonDecode(notificationData!.payload!));
+      return data;
+    }
+
+    return null;
+  }
+}
