3bc1c240bc2831ddd1075500ca0dfd86069978c7
Wahyu Hidayat <wahyu.smallker@gmail.com>
2022-07-24 22:20:13 +0700

    adding config

--- starter/__brick__/lib/core/config/config.dart
+++ starter/__brick__/lib/core/config/config.dart
@@ -1,4 +1,8 @@
 class Config {
   static const baseUrl = 'https://629867c8de3d7eea3c664c1a.mockapi.io';
   static const apiKey = 'API_KEY';
+  static const notificationChannelId = '{{package_name}}_channel_id';
+  static const notificationChannelName = '{{package_name}} notification';
+  static const notificationChannelDesc = '{{package_name}} notification';
+  static const savedNotification = 'FCM_MESSAGE';
 }
