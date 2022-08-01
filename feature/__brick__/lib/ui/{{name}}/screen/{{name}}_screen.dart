800f48bccad165a2ac1029e06b385138b95a8870
Wahyu Hidayat <wahyu.smallker@gmail.com>
2022-08-01 10:56:47 +0700

    feat: add feature brick

---
+++ feature/__brick__/lib/ui/{{name}}/screen/{{name}}_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class {{name.pascalCase()}}Screen extends StatelessWidget {
+  const {{name.pascalCase()}}Screen({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context){
+    return CustomScrollView();
+  }
+}
