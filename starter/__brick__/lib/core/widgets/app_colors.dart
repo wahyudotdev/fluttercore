388e29712fcf8ef7757fcc0c4d03b1152445bbc7
Wahyu Hidayat <wahyu.smallker@gmail.com>
2022-08-19 22:26:35 +0700

    feat: adding primary color swatch generator

--- starter/__brick__/lib/core/widgets/app_colors.dart
+++ starter/__brick__/lib/core/widgets/app_colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const secondary = Color(0xFF7AC241);
-  static const primary = Color(0xFF1767B3);
+  static const primary = Color(0x{{primary_color}});
+  static const secondary = Color(0x{{secondary_color}});
   static const textBlack = Color(0xFF030303);
   static const textGray = Color(0xFFBABABA);
   static const softGray = Color(0xFFF3F3F3);
@@ -10,16 +10,16 @@ class AppColors {
   static final primarySwatch = MaterialColor(
     primary.value,
     const {
-      50: Color(0xff155da1), //10%
-      100: Color(0xff12528f), //20%
-      200: Color(0xff10487d), //30%
-      300: Color(0xff0e3e6b), //40%
-      400: Color(0xff0c345a), //50%
-      500: Color(0xff092948), //60%
-      600: Color(0xff071f36), //70%
-      700: Color(0xff051524), //80%
-      800: Color(0xff020a12), //90%
-      900: Color(0xff000000), //100%
+      50: Color({{swatches_9}}), //10%
+      100: Color({{swatches_8}}), //20%
+      200: Color({{swatches_7}}), //30%
+      300: Color({{swatches_6}}), //40%
+      400: Color({{swatches_5}}), //50%
+      500: Color({{swatches_4}}), //60%
+      600: Color({{swatches_3}}), //70%
+      700: Color({{swatches_2}}), //80%
+      800: Color({{swatches_1}}), //90%
+      900: Color({{swatches_0}}), //100%
     },
   );
 }
--- starter/hooks/pre_gen.dart
+++ starter/hooks/pre_gen.dart
@@ -5,4 +5,20 @@ import 'package:mason/mason.dart';
 void run(HookContext context) {
   final packageName = Directory.current.path.split('/').last;
   context.vars = {...context.vars, 'package_name': packageName};
+  createColorSwatches(context);
+}
+
+void createColorSwatches(HookContext context) {
+  final colorVal = context.vars['primary_color'] as String;
+  final r = int.parse((colorVal[2] + colorVal[3]), radix: 16);
+  final g = int.parse((colorVal[4] + colorVal[5]), radix: 16);
+  final b = int.parse((colorVal[6] + colorVal[7]), radix: 16);
+
+  for (var i = 0; i < 10; i++) {
+    final newR = (r + ((255 - r) * (0.1 * i))).toInt().toRadixString(16);
+    final newG = (g + ((255 - g) * (0.1 * i))).toInt().toRadixString(16);
+    final newB = (b + ((255 - b) * (0.1 * i))).toInt().toRadixString(16);
+    context.vars = {...context.vars, 'swatches_$i': '0xff$newR$newG$newB'};
+    context.logger.info(context.vars.toString());
+  }
 }
