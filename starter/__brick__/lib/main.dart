4fb8113448dd0d5c0ceef7623c34085271c1d100
wahyudotdev <wahyudotdev@gmail.com>
2022-12-26 11:08:05 +0700

    ref: clear unused dependency

--- starter/__brick__/lib/main.dart
+++ starter/__brick__/lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:path_provider/path_provider.dart';
 import 'core/utils/self_sign_cert.dart';
 import 'core/widgets/app_theme.dart';
 
